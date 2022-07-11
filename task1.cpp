80290b0b09f03c2865d91cb2185ee696d9077bd2
Vladislav Nikolaev <yavl98@mail.ru>
2022-07-11 18:55:29 +0900

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+
+// An imaginary library meant to handle string manipulation
+namespace ImaginaryLibrary {
+    std::string lowercased(const std::string& text) {
+        std::string lowercased;
+        for (auto& character : text) {
+            lowercased += std::tolower(character);
+        }
+        return lowercased;
+    }
+};
+
+std::string getConvertedText(const std::string& sourceText) {
+    std::unordered_map<char, int> charactersCount;
+    std::string lowercased = ImaginaryLibrary::lowercased(sourceText);
+    for (const auto& character : lowercased) {
+        ++charactersCount[character];
+    }
+    std::string convertedText = "";
+    for (const auto& character : lowercased) {
+        convertedText += charactersCount[character] == 1 ? "(" : ")";
+    }
+    return convertedText;
+}
+
+int main() {
+    std::cout << getConvertedText("din") << std::endl;
+    std::cout << getConvertedText("recede") << std::endl;
+    std::cout << getConvertedText("Success") << std::endl;
+    std::cout << getConvertedText("(( @") << std::endl;
+}
